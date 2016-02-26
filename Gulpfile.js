2d3cad552d75633eae248559e3e299fcd21c3ac3
Zach Schneider <zach@schneid.io>
2016-02-26 14:47:33 -0600

    Better Gulpfile formatting

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,29 +8,41 @@ var minifyHTML = require('gulp-minify-html');
 var connect    = require('gulp-connect');
 
 gulp.task('css', function() {
-  return gulp.src('css/**/*').pipe(concat('production.min.css')).pipe(minifyCSS({
-    advanced: false
-  })).pipe(gulp.dest('dist'));
+  return gulp.src('css/**/*')
+    .pipe(concat('production.min.css'))
+    .pipe(minifyCSS({ advanced: false }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('cssDev', function() {
-  return gulp.src('css/**/*').pipe(concat('production.min.css')).pipe(gulp.dest('dist'));
+  return gulp.src('css/**/*')
+    .pipe(concat('production.min.css'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
-  return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/raphael/raphael.js', 'js/jquery.usmap.js', 'js/app.js']).pipe(concat('production.min.js')).pipe(uglify()).pipe(gulp.dest('dist'));
+  return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/raphael/raphael.js', 'js/jquery.usmap.js', 'js/app.js'])
+    .pipe(concat('production.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('jsDev', function() {
-  return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/raphael/raphael.js', 'js/jquery.usmap.js', 'js/app.js']).pipe(concat('production.min.js')).pipe(gulp.dest('dist'));
+  return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/raphael/raphael.js', 'js/jquery.usmap.js', 'js/app.js'])
+    .pipe(concat('production.min.js'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy', function() {
-  return gulp.src('index.html').pipe(copy('dist'));
+  return gulp.src('index.html')
+    .pipe(copy('dist'));
 });
 
 gulp.task('inline', ['copy'], function() {
-  return gulp.src('dist/index.html').pipe(inline()).pipe(minifyHTML()).pipe(gulp.dest('dist'));
+  return gulp.src('dist/index.html')
+    .pipe(inline())
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
